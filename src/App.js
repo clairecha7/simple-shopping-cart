bac842f45cec94d15b0c7d66053ef0a2b272ac9e
clairecha7 <mybarclaire@gmail.com>
2023-05-19 15:29:53 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react"
+import { data } from "./data"
+import "./App.css"
 function App() {
+  const [products, setData] = useState(data)
+  const [total, setTotal] = useState(0)
+  const [search, setSearch] = useState("")
+  const handleClick = (id, email) => {
+    // const filtered = names.filter(i => i.id != id)
+
+    // setData(filtered)
+  }
+  const handleDelete = (id) => {
+    const filtered = products.filter(item => item.id != id)
+
+    let total = 0
+    for (let i of filtered) {
+      total = total + i.count * i.price
+    }
+    setTotal(total)
+    setData(filtered)
+
+  }
+  const handleIncrement = (id) => {
+    const newData = [...products]
+    for (let i of newData) {
+      if (i.id === id) {
+        i.count++
+      }
+    }
+    let total = 0
+    for (let i of newData) {
+      total = total + i.count * i.price
+      console.log(total)
+    }
+    setTotal(total)
+    setData(newData)
+
+  }
+  const handleDecrement = (id) => {
+    const newData = [...products]
+    for (let i of newData) {
+      if (i.id === id && i.count > 0) {
+        i.count--
+      }
+    }
+    let total = 0
+    for (let i of newData) {
+      total = total - i.count * i.price
+      console.log(total)
+    }
+    setTotal(total)
+    setData(newData)
+  }
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+  const afterSearch = products.filter(item => item.name.toLocaleLowerCase().includes(search))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h3>Shopping cart</h3>
+      </div>
+      <div className="search">
+        <input type="text" placeholder="search..." onChange={handleSearch} />
+      </div>
+      {
+        afterSearch.map(product => {
+          return (
+            <div className="product">
+              <img src={product.image} alt={product.name} />
+              <p>{product.name} <br /> <span className="price">{product.price}$</span>
+                <br />
+                <span className="price">{product.count}</span>
+                <br />
+                <button className="price" onClick={() => handleDelete(product.id)}>delete</button>
+              </p>
+              <div className="incrDecr">
+                <button onClick={() => handleIncrement(product.id)}>+</button>
+                <button onClick={() => handleDecrement(product.id)}>-</button>
+              </div>
+            </div>
+          )
+        })
+      }
+      <div>
+        {products.length === 0 ? <h2>No items left</h2> : ""}
+      </div>
+      <div className="footer">
+        <h3>Total: {total} $</h3>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    id: 55,
+    name: "Iphone X",
+    price: 199,
+    count: 0,
+    image:
+      "https://c0.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2017/11/apple-iphone-x-par-frandroid-1-1000x750.jpg?key=34de3a93",
+  },
+  {
+    id: 15,
+    name: "Samsung",
+    price: 399,
+    count: 0,
+    image:
+      "https://media.cnn.com/api/v1/images/stellar/prod/z-flip-tilted.jpg?c=16x9&q=h_270,w_480,c_fill",
+  },
+  {
+    id: 0,
+    name: "Nokia lumia",
+    price: 250,
+    count: 0,
+    image:
+      "https://www.cnet.com/a/img/resize/75a47aa8ecd6f2102307d61a7bbe37e536d52375/hub/2014/07/18/8911898c-3122-48d3-9bc5-63f4e89ca34d/nokia-lumia-930-product.jpg?auto=webp&fit=crop&height=675&width=1200",
+  },
+  {
+    id: 5,
+    name: "Motorola",
+    price: 150,
+    count: 0,
+    image:
+      "https://www.zdnet.com/a/img/resize/551c979400ac0ec2430ea5f0ea39ecf5f1f201b7/2022/08/17/691268ec-f438-46f0-903b-de8e43e3c637/moto-edge-2022-1.jpg?auto=webp&fit=crop&height=1200&width=1200",
+  },
+];
